5041ce0a1c681c3f0e8e7fc5274f6ea15aa990ee
Nando PL <nandopiless@gmail.com>
2024-02-14 13:01:01 +0100

    jumbotron creado

---
+++ my-app/src/app/components/jumbotron/jumbotron.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jumbotron',
+  standalone: true,
+  imports: [],
+  templateUrl: './jumbotron.component.html',
+  styleUrl: './jumbotron.component.css'
+})
+export class JumbotronComponent {
+
+}
