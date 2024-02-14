2ab499964af32f21fe6820f0b4e146a54d90153c
Nando PL <nandopiless@gmail.com>
2024-02-14 13:10:52 +0100

    header links hechos, únicamente existe la vista Index

--- my-app/src/app/components/header/header.component.ts
+++ my-app/src/app/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
