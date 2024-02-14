e5e65134452deec4c98fe20f3ea66060473dfc64
Nando PL <nandopiless@gmail.com>
2024-02-14 12:16:01 +0100

    Vista index creada y creación componente Header sin rutas

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { IndexComponent } from './views/index/index.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, IndexComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
