3f03793c35ba62fee2a310f8908ca0895d52787e
mojial <mojial@floridauniversitaria.es>
2024-02-15 12:31:19 +0100

    added galeria page

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -3,13 +3,13 @@ import { IndexComponent } from './views/index/index.component';
 import { CancerComponent } from './components/cancer/cancer.component';
 import { NosotrosComponent } from './components/nosotros/nosotros.component';
 import { NoticiasComponent } from './components/noticias/noticias.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: IndexComponent },
     { path: 'cancer', component: CancerComponent },
     { path: 'about', component: NosotrosComponent},
-    { path: 'news', component: NoticiasComponent }
-
-
+    { path: 'news', component: NoticiasComponent },
+    {path: 'gallery', component: GaleriaComponent}
 ];
---
+++ my-app/src/app/components/galeria/galeria.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ImagenesGaleriaComponent } from "../imagenes-galeria/imagenes-galeria.component";
+
+@Component({
+    selector: 'app-galeria',
+    standalone: true,
+    templateUrl: './galeria.component.html',
+    styleUrl: './galeria.component.css',
+    imports: [ImagenesGaleriaComponent]
+})
+export class GaleriaComponent {
+
+}
