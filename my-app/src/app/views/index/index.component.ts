c355e2569322d2cacb7a0e677d67e0b4a5bce3e4
Nando PL <nandopiless@gmail.com>
2024-02-15 09:22:37 +0100

    footer creado

--- my-app/src/app/views/index/index.component.ts
+++ my-app/src/app/views/index/index.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { JumbotronComponent } from '../../components/jumbotron/jumbotron.component';
 import { ImgIndexCardComponent } from '../../components/index/img-index-card/img-index-card.component';
 import { DescriptionIndexCardComponent } from '../../components/index/description-index-card/description-index-card.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [JumbotronComponent, ImgIndexCardComponent, DescriptionIndexCardComponent],
+  imports: [JumbotronComponent, ImgIndexCardComponent, DescriptionIndexCardComponent, FooterComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
