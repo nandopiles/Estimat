ec894ce55399a9ae6202eda3b9d9ad2af47068e0
mojial <mojial@floridauniversitaria.es>
2024-02-15 10:20:53 +0100

    added about us page

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './views/index/index.component';
 import { CancerComponent } from './components/cancer/cancer.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: IndexComponent },
-    { path: 'cancer', component: CancerComponent }
+    { path: 'cancer', component: CancerComponent },
+    { path: 'about', component: NosotrosComponent }
+
 ];
