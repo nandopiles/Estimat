cdb11177f4bd52cf60e7eccc571d7db0d16898d2
mojial <mojial@floridauniversitaria.es>
2024-02-16 09:47:53 +0100

    fixed views

--- my-app/src/app/views/nosotros/nosotros.component.ts
+++ my-app/src/app/views/nosotros/nosotros.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
+import { HeaderComponent } from "../../components/header/header.component";
 
 @Component({
     selector: 'app-nosotros',
--- my-app/src/app/views/noticias/noticias.component.ts
+++ my-app/src/app/views/noticias/noticias.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { JumbotronNoticiasComponent } from "../jumbotron-noticias/jumbotron-noticias.component";
-import { CardNoticiasComponent } from "../card-noticias/card-noticias.component";
+import { JumbotronNoticiasComponent } from "../../components/jumbotron-noticias/jumbotron-noticias.component";
+import { CardNoticiasComponent } from "../../components/card-noticias/card-noticias.component";
 
 @Component({
     selector: 'app-noticias',
