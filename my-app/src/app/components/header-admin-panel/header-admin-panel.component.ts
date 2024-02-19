392d8162e1f3773301493830736c13da16b0c6db
Nando PL <nandopiless@gmail.com>
2024-02-19 13:18:55 +0100

    scripts agregados y componente headerAdminPanel creado

---
+++ my-app/src/app/components/header-admin-panel/header-admin-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-admin-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-admin-panel.component.html',
+  styleUrl: './header-admin-panel.component.css'
+})
+export class HeaderAdminPanelComponent {
+
+}
