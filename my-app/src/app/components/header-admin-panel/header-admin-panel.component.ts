cf343c4367e2445bb58203fde384030ff4d47cc7
Nando PL <nandopiless@gmail.com>
2024-02-20 08:40:58 +0100

    css movido a componente headerAdminPanel y componente hecho html

--- my-app/src/app/components/header-admin-panel/header-admin-panel.component.ts
+++ my-app/src/app/components/header-admin-panel/header-admin-panel.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header-admin-panel',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header-admin-panel.component.html',
   styleUrl: './header-admin-panel.component.css'
 })
