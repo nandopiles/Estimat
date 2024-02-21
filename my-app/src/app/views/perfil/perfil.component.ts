418618483cd0c70bde40b087793eb17056a6094a
jesus <jyjmatet@gmail.com>
2024-02-21 11:10:36 +0100

    perfil completado

--- my-app/src/app/views/perfil/perfil.component.ts
+++ my-app/src/app/views/perfil/perfil.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
@@ -9,5 +9,14 @@ import { HeaderComponent } from '../../components/header/header.component';
   styleUrl: './perfil.component.css'
 })
 export class PerfilComponent {
+  @ViewChild('passwordInput') passwordInput!: ElementRef<HTMLInputElement>;
 
+  togglePasswordVisibility(): void {
+    const inputType = this.passwordInput.nativeElement.type;
+    if (inputType === 'password') {
+      this.passwordInput.nativeElement.type = 'text';
+    } else {
+      this.passwordInput.nativeElement.type = 'password';
+    }
+  }
 }
