e4fa4979ddf7cfa610b615254685ae10a1304665
Nando PL <nandopiless@gmail.com>
2024-02-22 19:08:58 +0100

    estructura galería cambiada y comunicación padre e hijo creada

--- my-app/src/app/views/galeria/galeria.component.ts
+++ my-app/src/app/views/galeria/galeria.component.ts
@@ -9,5 +9,68 @@ import { ImagenesGaleriaComponent } from "../../components/imagenes-galeria/imag
     imports: [ImagenesGaleriaComponent]
 })
 export class GaleriaComponent {
-
+    public imagesGallery: string[][] = [
+        [
+            '/assets/imagesGallery/gallery.jpg',
+            '/assets/imagesGallery/gallery2.jpg',
+            '/assets/imagesGallery/gallery3.jpg',
+            '/assets/imagesGallery/gallery4.jpg',
+        ],
+        [
+            '/assets/imagesGallery/gallery5.jpg',
+            '/assets/imagesGallery/gallery6.jpg',
+            '/assets/imagesGallery/gallery7.jpg',
+            '/assets/imagesGallery/gallery8.jpg',
+        ],
+        [
+            '/assets/imagesGallery/gallery9.jpg',
+            '/assets/imagesGallery/gallery10.jpg',
+            '/assets/imagesGallery/gallery11.jpg',
+            '/assets/imagesGallery/gallery12.jpg',
+        ],
+        [
+            '/assets/imagesGallery/gallery13.jpg',
+            '/assets/imagesGallery/gallery14.jpg',
+            '/assets/imagesGallery/gallery15.jpg',
+            '/assets/imagesGallery/gallery16.jpg',
+        ],
+        [
+            '/assets/imagesGallery/gallery17.jpg',
+            '/assets/imagesGallery/gallery18.jpg',
+            '/assets/imagesGallery/gallery19.jpg',
+            '/assets/imagesGallery/gallery20.jpg',
+        ],
+        [
+            '/assets/imagesGallery/gallery21.jpg',
+            '/assets/imagesGallery/gallery22.jpg',
+            '/assets/imagesGallery/gallery23.jpg',
+            '/assets/imagesGallery/gallery24.jpg',
+        ],
+    ];
+    /* public imagesGallery: string[] = [
+        '/assets/imagesGallery/gallery.jpg',
+        '/assets/imagesGallery/gallery2.jpg',
+        '/assets/imagesGallery/gallery3.jpg',
+        '/assets/imagesGallery/gallery4.jpg',
+        '/assets/imagesGallery/gallery5.jpg',
+        '/assets/imagesGallery/gallery6.jpg',
+        '/assets/imagesGallery/gallery7.jpg',
+        '/assets/imagesGallery/gallery8.jpg',
+        '/assets/imagesGallery/gallery9.jpg',
+        '/assets/imagesGallery/gallery10.jpg',
+        '/assets/imagesGallery/gallery11.jpg',
+        '/assets/imagesGallery/gallery12.jpg',
+        '/assets/imagesGallery/gallery13.jpg',
+        '/assets/imagesGallery/gallery14.jpg',
+        '/assets/imagesGallery/gallery15.jpg',
+        '/assets/imagesGallery/gallery16.jpg',
+        '/assets/imagesGallery/gallery17.jpg',
+        '/assets/imagesGallery/gallery18.jpg',
+        '/assets/imagesGallery/gallery19.jpg',
+        '/assets/imagesGallery/gallery20.jpg',
+        '/assets/imagesGallery/gallery21.jpg',
+        '/assets/imagesGallery/gallery22.jpg',
+        '/assets/imagesGallery/gallery23.jpg',
+        '/assets/imagesGallery/gallery24.jpg'
+    ] */
 }
