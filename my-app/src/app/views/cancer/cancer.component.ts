1845f35ac3899c1a6b6dbc7d03c4dca0e5a1cb99
mojial <mojial@floridauniversitaria.es>
2024-02-23 10:27:29 +0100

    optimización visual

--- my-app/src/app/views/cancer/cancer.component.ts
+++ my-app/src/app/views/cancer/cancer.component.ts
@@ -18,8 +18,7 @@ export class CancerComponent {
             text: `Es una enfermedad maligna que se origina en las células de la
             glándula mamaria.Se caracteriza por un crecimiento anormal y descontrolado de estas células,
             formando un tumor que puede propagarse a tejidos cercanos o a otras partes del cuerpo a través
-            del sistema linfático o sanguíneo.Es una de las formas más comunes de cáncer entre las mujeres,
-            aunque también puede afectar a los hombres en menor medida.`,
+            del sistema linfático o sanguíneo.`,
             img: '/assets/img/pexels-photo-cd.png'
         },
         {
@@ -27,10 +26,7 @@ export class CancerComponent {
             text: `El tratamiento del cáncer de mama depende de varios factores, incluyendo el
             tipo de cáncer, su estadio, y la salud general de la paciente. Las opciones de tratamiento
             suelen incluir cirugía para extirpar el tumor, radioterapia para destruir las células
-            cancerosas, quimioterapia para eliminar las células cancerosas en todo el cuerpo, terapia
-            hormonal para bloquear el crecimiento de las células cancerosas que son estimuladas por
-            hormonas, y terapia dirigida que ataca específicamente las células cancerosas. El tratamiento
-            puede ser combinación de estas opciones o individual, dependiendo del caso.`,
+            cancerosas, quimioterapia para eliminar las células cancerosas en todo el cuerpo.`,
             img: '/assets/img/pexels-photo2.png'
         },
         {
@@ -38,10 +34,7 @@ export class CancerComponent {
             text: `El cáncer de mama puede tener un impacto significativo en la vida de quienes lo
             padecen y en sus seres queridos. Además del aspecto físico y médico, puede generar emociones
             como miedo, ansiedad, tristeza y frustración. El tratamiento puede ser agotador física y
-            emocionalmente, y las preocupaciones sobre la reaparición del cáncer son frecuentes. La
-            enfermedad también puede afectar las relaciones personales, la vida laboral y la autoestima. Es
-            fundamental contar con un sólido sistema de apoyo emocional y médico para hacer frente a los
-            desafíos que presenta el cáncer de mama.`,
+            emocionalmente, y las preocupaciones sobre la reaparición del cáncer son frecuentes.`,
             img: '/assets/img/pexels-cupcakes.jpg'
         }
     ]
