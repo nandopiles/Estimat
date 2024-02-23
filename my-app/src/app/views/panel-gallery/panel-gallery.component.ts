78bce141c317dbdcdd077f763696ad562dc41d26
mojial <mojial@floridauniversitaria.es>
2024-02-23 09:10:26 +0100

    video index

--- my-app/src/app/views/panel-gallery/panel-gallery.component.ts
+++ my-app/src/app/views/panel-gallery/panel-gallery.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-panel-gallery',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './panel-gallery.component.html',
   styleUrl: './panel-gallery.component.css'
 })
