d4f573ffe1edc7a6a87e80ef0c01a670abe1c2c3
Aikarubi <nerupe@floridauniversitaria.es>
2024-02-23 13:47:11 +0100

    typescript del form

--- my-app/src/app/components/form/form.component.ts
+++ my-app/src/app/components/form/form.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+
+reactiveForm = new FormGroup({
+  email: new FormControl(''),
+  password: new FormControl('')
+});
+
+public onSubmit(): void {
+  console.log('form: ', this.reactiveForm);
+  console.log('form: ', this.reactiveForm.value);
+  console.log('form: ', this.reactiveForm.value.password);
+}
 
 }
