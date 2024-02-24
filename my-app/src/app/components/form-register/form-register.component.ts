e60b03e7892e2480d20f256ed833173ab8882140
Aikarubi <nerupe@floridauniversitaria.es>
2024-02-24 13:18:00 +0100

    Muestra los datos por consola pero no inserta

--- my-app/src/app/components/form-register/form-register.component.ts
+++ my-app/src/app/components/form-register/form-register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-form-register',
@@ -11,18 +12,35 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class FormRegisterComponent {
 
+  public urlApi: string = 'http://localhost:8000/'; 
+
   reactiveForm = new FormGroup({
     email: new FormControl(''),
     password: new FormControl(''),
     name: new FormControl(''),
     repeatPassword: new FormControl(''),
     cancer: new FormControl('')
-
   });
-  
+
+  constructor(private http: HttpClient) {} 
+
   public onSubmit(): void {
     console.log('form: ', this.reactiveForm);
     console.log('form: ', this.reactiveForm.value);
     console.log('form: ', this.reactiveForm.value.password);
+  }
+
+  public insertUser(): void {
+    const urlInsertUser = `${this.urlApi}insert/user`; 
+
+    // Envía la solicitud POST al servidor
+    this.http.post(urlInsertUser, this.reactiveForm.value).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
   }
 }
