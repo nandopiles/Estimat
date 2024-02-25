3414bfdcb9acaf20c65b498535c8f14bf906094b
jesus <jyjmatet@gmail.com>
2024-02-25 16:51:46 +0100

    panel de la galeria

--- my-app/src/app/app.config.ts
+++ my-app/src/app/app.config.ts
@@ -6,5 +6,5 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideHttpClient(), provideAnimationsAsync(), provideAnimationsAsync()]
 };
--- my-app/src/app/views/panel-gallery/panel-gallery.component.ts
+++ my-app/src/app/views/panel-gallery/panel-gallery.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { EstimatService } from '../../services/estimat.service';
 
 @Component({
   selector: 'app-panel-gallery',
@@ -9,32 +10,15 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './panel-gallery.component.css'
 })
 export class PanelGalleryComponent {
-  public imagesGallery: string[] = [
-    '../../../assets/imagesGallery/gallery.jpg',
-    '../../../assets/imagesGallery/gallery2.jpg',
-    '../../../assets/imagesGallery/gallery3.jpg',
-    '../../../assets/imagesGallery/gallery4.jpg',
-    '../../../assets/imagesGallery/gallery5.jpg',
-    '../../../assets/imagesGallery/gallery6.jpg',
-    '../../../assets/imagesGallery/gallery7.jpg',
-    '../../../assets/imagesGallery/gallery8.jpg',
-    '../../../assets/imagesGallery/gallery9.jpg',
-    '../../../assets/imagesGallery/gallery10.jpg',
-    '../../../assets/imagesGallery/gallery11.jpg',
-    '../../../assets/imagesGallery/gallery12.jpg',
-    '../../../assets/imagesGallery/gallery13.jpg',
-    '../../../assets/imagesGallery/gallery14.jpg',
-    '../../../assets/imagesGallery/gallery15.jpg',
-    '../../../assets/imagesGallery/gallery16.jpg',
-    '../../../assets/imagesGallery/gallery17.jpg',
-    '../../../assets/imagesGallery/gallery18.jpg',
-    '../../../assets/imagesGallery/gallery19.jpg',
-    '../../../assets/imagesGallery/gallery20.jpg',
-    '../../../assets/imagesGallery/gallery21.jpg',
-    '../../../assets/imagesGallery/gallery22.jpg',
-    '../../../assets/imagesGallery/gallery23.jpg',
-    '../../../assets/imagesGallery/gallery24.jpg'
-  ]
-  
+
+
+
   public deleteButon: any;
+
+  public constructor (public _estimatService: EstimatService){}
+
+  deletePhoto(index: number) {
+      this._estimatService.getImages().splice(index, 1); // Elimina la foto del arreglo
+  }
+
 }
