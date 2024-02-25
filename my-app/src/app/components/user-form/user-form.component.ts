9b826bb3ed5835844f6c518b72898e18e7a8f0b8
Nando PL <nandopiless@gmail.com>
2024-02-25 13:11:06 +0100

    método updateUser funcionando correctamente con spinner de carga en todas sus vistas

--- my-app/src/app/components/user-form/user-form.component.ts
+++ my-app/src/app/components/user-form/user-form.component.ts
@@ -1,18 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { IUser } from '../../interfaces/estimat.interface';
 import { EstimatService } from '../../services/estimat.service';
+import { ThemePalette } from '@angular/material/core';
+import { MatProgressSpinnerModule, ProgressSpinnerMode } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-user-form',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink, RouterLinkActive],
+  imports: [ReactiveFormsModule, RouterLink, RouterLinkActive, MatProgressSpinnerModule],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.css'
 })
 export class UserFormComponent implements OnInit {
   @Input() userSelected: any = {};
+  public loading: boolean = false;
+  public colorSpinner: ThemePalette = "accent";
+  public mode: ProgressSpinnerMode = 'indeterminate';
 
   public reactiveForm = new FormGroup({
     name: new FormControl(''),
@@ -22,7 +27,10 @@ export class UserFormComponent implements OnInit {
     role: new FormControl(false),
   });
 
-  public constructor(public _estimatService: EstimatService) { }
+  public constructor(
+    public _estimatService: EstimatService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   private populateForm(): void {
     this.reactiveForm.setValue({
@@ -32,6 +40,7 @@ export class UserFormComponent implements OnInit {
       status: this.userSelected.status || false,
       role: this.userSelected.role || false,
     });
+    this.loading = true;
   }
 
   /**
@@ -42,6 +51,20 @@ export class UserFormComponent implements OnInit {
     this.populateForm();
   }
 
+  /**
+   * Redirects to the url passed by parameter.
+   * @param {string} redirectUrl
+   * @returns {void}
+   */
+  private handleSuccessAndRedirect(redirectUrl: string): void {
+    this.loading = false;
+    this.router.navigate([redirectUrl], { relativeTo: this.route });
+  }
+
+  /**
+   * Makes the changes persisted in the database.
+   * @returns {void}
+   */
   public onSubmit(): void {
     const userCreated: IUser = {
       id: this.userSelected.id,
@@ -51,11 +74,19 @@ export class UserFormComponent implements OnInit {
       status: this.reactiveForm.value.status ?? false,
       role: this.reactiveForm.value.role ?? false
     }
+    this.loading = false;
 
     if (this.userSelected.id === 0) { // is an insert
       this._estimatService.insertUser(userCreated).subscribe((data) => console.log(data));
     } else { // is a modification of an existing User
-      this._estimatService.updateUser(userCreated).subscribe((data) => console.log(data));
+      this._estimatService.updateUser(userCreated).subscribe(
+        (data) => {
+          console.log(data);
+        },
+        (error) => {
+          this.handleSuccessAndRedirect('/usersManagement');
+        }
+      );
     }
   }
 }
