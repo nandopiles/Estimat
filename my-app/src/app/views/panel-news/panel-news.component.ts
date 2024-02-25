582266b02aa20f6dff6279b0167600539bc7b1ef
mojial <mojial@floridauniversitaria.es>
2024-02-25 12:12:04 +0100

    añadido insertar una noticia

--- my-app/src/app/views/panel-news/panel-news.component.ts
+++ my-app/src/app/views/panel-news/panel-news.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { INew } from '../../interfaces/estimat.interface';
 import { EstimatService } from '../../services/estimat.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-panel-news',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, ReactiveFormsModule],
   templateUrl: './panel-news.component.html',
   styleUrl: './panel-news.component.css'
 })
@@ -16,13 +17,20 @@ public news: INew[] = [];
 
 constructor(private _estimatService: EstimatService) { }
 
+  newsForm = new FormGroup({
+    id: new FormControl('0'),
+    title: new FormControl(''),
+    description: new FormControl(''),
+    image: new FormControl(''),
+  });
+  
+
+createNew():void{
+  const newsData = this.newsForm.value;
+  this._estimatService.insertNews(newsData).subscribe();
+}
+
 ngOnInit(): void {
   
 }
-
-create():void{
-  console.log(this.news);
-}
-
-
 }
